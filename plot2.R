f3825272b15beb0a8050179e5c20fbbfab2aed42
Osman Esquivel Gonzalez <Egosman@users.noreply.github.com>
2014-12-07 14:08:43 -0600

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,5 +1,5 @@
-file <- "./data/household_power_consumption.txt"
-data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+file <- "C:/Users/Osman Esquivel Gonza/Documents/Archivos R/household_power_consumption.txt"
+data <- read.table(file, header = TRUE, sep = ";", stringsAsFactors = FALSE, dec = ".")
 subData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
 #str(subSetData)
